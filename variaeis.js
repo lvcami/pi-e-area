5844cd529e9278a8578b2ae444a6176a6912b7f8
lvcami <163419225+lvcami@users.noreply.github.com>
2024-12-03 12:41:18 -0300

    Add files via upload

---
+++ variaeis.js
@@ -0,0 +1,6 @@
+let raio = 5; // definindo o raio
+let area = Math.PI * raio * raio; // calculando a area
+console.log(area); // imprime a area
+raio = 10; // alterando o valor do raio
+area = Math.PI * raio * raio; // recalculando a area
+console.log(area); // imprime a nova area
